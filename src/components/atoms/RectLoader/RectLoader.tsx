de54d0bde55ec6c9941e5b32d76c40a75cd2c4f4
shomrkm <shochan1985@gmail.com>
2023-06-15 10:45:51 +0900

    feat: Add RectLoader component.

---
+++ src/components/atoms/RectLoader/RectLoader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './RectLoader.css';
+
+export const RectLoader = () => {
+  return (
+    <div className="rect-loader">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
