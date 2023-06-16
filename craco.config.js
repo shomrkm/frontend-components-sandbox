97b1b4965b55f13d2e6715d7968556694b5edb24
shomrkm <shochan1985@gmail.com>
2023-06-16 14:51:40 +0900

    chore: add settings for emotion.

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    babel: {
+        presets: [
+            [
+                "@babel/preset-react",
+                { runtime: "automatic", importSource: "@emotion/react" },
+            ],
+        ],
+        plugins: ["@emotion/babel-plugin"],
+    },
+    webpack: {
+        alias: {
+            '@': path.resolve(__dirname, './src/'),
+        },
+    }
+};
