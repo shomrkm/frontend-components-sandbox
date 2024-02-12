d3f51592876f62fdb47c3e0d76d6c67dc353d859
shomrkm <shochan1985@gmail.com>
2024-02-12 11:26:30 +0900

    [DragBoard] Storybook のテストデータを更新

--- src/components/organisms/DragBoard/DragBoard.stories.tsx
+++ src/components/organisms/DragBoard/DragBoard.stories.tsx
@@ -34,11 +34,43 @@ export const Default: Story = {
       },
       {
         id: uuidv4(),
-        data: { name: '須磨 英子', rank: 'C', img: 'https://iili.io/J146hUF.png' },
+        data: { name: '須磨 英子', rank: 'A', img: 'https://iili.io/J146hUF.png' },
       },
       {
         id: uuidv4(),
-        data: { name: '須磨 英知', rank: 'C', img: 'https://iili.io/J14P2bj.png' },
+        data: { name: '須磨 英知', rank: '未設定', img: 'https://iili.io/J14P2bj.png' },
+      },
+      {
+        id: uuidv4(),
+        data: { name: '草野 栄一郎', rank: 'B', img: 'https://iili.io/J14pbS4.png' },
+      },
+      {
+        id: uuidv4(),
+        data: { name: '岩下 香澄', rank: 'B', img: 'https://iili.io/J14y9NS.png' },
+      },
+      {
+        id: uuidv4(),
+        data: { name: '島袋 月代', rank: 'B', img: 'https://iili.io/J169bu2.png' },
+      },
+      {
+        id: uuidv4(),
+        data: { name: '永山 侑太郎', rank: 'B', img: 'https://iili.io/J16HquV.png' },
+      },
+      {
+        id: uuidv4(),
+        data: { name: '上原 玲子', rank: 'C', img: 'https://iili.io/J16Hon1.png' },
+      },
+      {
+        id: uuidv4(),
+        data: { name: 'Robert Smith', rank: 'C', img: 'https://iili.io/J16HTFa.png' },
+      },
+      {
+        id: uuidv4(),
+        data: { name: '大和 真', rank: 'D', img: 'https://iili.io/J16HatI.png' },
+      },
+      {
+        id: uuidv4(),
+        data: { name: '松原 英太', rank: 'D', img: 'https://iili.io/J16Hlnt.png' },
       },
     ],
     onUpdate: (data: User[]) => {
@@ -49,7 +81,7 @@ export const Default: Story = {
   decorators: [
     (Story) => {
       return (
-        <div style={{ margin: '1rem', backgroundColor: '#F7F6F4' }}>
+        <div style={{ margin: '0.25rem', padding: '1rem', backgroundColor: '#F7F6F4' }}>
           <Story />
         </div>
       );
