82421b865e8ee31ac24adc0d5f322b04e22464ca
shomrkm <shochan1985@gmail.com>
2024-11-24 11:53:26 +0900

    test

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -58,9 +58,38 @@ export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ type = 'button', variant = 'primary', size = 'sm', ...props }, ref) => {
     return (
-      <button {...props} ref={ref} type={type} css={[style, variants[variant], sizes[size]]}>
-        <span>{props.children}</span>
-      </button>
+      <>
+        <button {...props} ref={ref} type={type} css={[style, variants[variant], sizes[size]]}>
+          <span>{props.children}</span>
+        </button>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+        <div>duplication test</div>
+      </>
     );
   }
 );
