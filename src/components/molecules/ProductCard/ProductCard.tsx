747b32104cee7a950ba678cbf998ef76aae5a1c0
shomrkm <shochan1985@gmail.com>
2024-12-08 13:16:27 +0900

    fix codeclimate issues

--- src/components/molecules/ProductCard/ProductCard.tsx
+++ src/components/molecules/ProductCard/ProductCard.tsx
@@ -44,14 +44,6 @@ function ProductCard({ product, image, info, action }: Props) {
           {info}
           {action}
         </div>
-        <div css={bottomStyle}>
-          {info}
-          {action}
-        </div>
-        <div css={bottomStyle}>
-          {info}
-          {action}
-        </div>
       </div>
     </ProductCardContext.Provider>
   );
