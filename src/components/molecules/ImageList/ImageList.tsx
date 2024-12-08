ad404d9233b4ba4c08a67bfdc383475f683e29e3
shomrkm <shochan1985@gmail.com>
2024-12-08 12:57:49 +0900

    add duplicated codes.

--- src/components/molecules/ImageList/ImageList.tsx
+++ src/components/molecules/ImageList/ImageList.tsx
@@ -17,6 +17,16 @@ export const ImageList: FC<Props> = ({ images }) => {
           <img src={image.src} alt={image.alt} />
         </div>
       ))}
+      {images.map((image) => (
+        <div key={image.id} className="item">
+          <img src={image.src} alt={image.alt} />
+        </div>
+      ))}
+      {images.map((image) => (
+        <div key={image.id} className="item">
+          <img src={image.src} alt={image.alt} />
+        </div>
+      ))}
     </div>
   );
 };
